bff3719ace3bc6e34b436103fd1d3955dd04e975
savci <highcsavci@gmail.com>
2024-08-06 05:19:38 +0300

    first commit

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from sam.router.aging_router import router as sam_aging_router
-from adfd.router.aging_router import router as adfd_aging_router
+from backend.sam.router.aging_router import router as sam_aging_router
+from backend.adfd.router.aging_router import router as adfd_aging_router
 from log.config import setup_logging
 
 app = FastAPI()
 setup_logging()
 app.include_router(sam_aging_router)
 app.include_router(adfd_aging_router)
-
