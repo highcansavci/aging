4668b74f6a1d712ba19d087588833a5162d61c7f
Can Savcı <43791867+highcansavci@users.noreply.github.com>
2024-08-08 22:12:23 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Determine the absolute path to the project root
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Append the project root to sys.path
+sys.path.append(project_root)
+
 from backend.log.config import setup_logging
 from backend.adfd.router.aging_router import router as adfd_aging_router
 from backend.sam.router.aging_router import router as sam_aging_router
